d51ed66af497a8c8c43b2a3084d14532c6eaf9ae
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-06-09 14:08:09 +0630

    update category

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function categories(){
+        return $this->hasMany(Category::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
