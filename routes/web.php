bb5846f101e63c51c1794b66f697fecee5d2f21f
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-06-09 13:37:23 +0630

    blog finish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('article', ArticleController::class);
+Route::resource("category",CategoryController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
