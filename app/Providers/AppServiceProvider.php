9742ba965db1fa6677d01efec21e192c4de46537
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-06-09 14:05:09 +0630

    update article

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Article;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Paginator::useBootstrapFive();
+
+        View::share('recentArticles',Article::latest("id")->limit(5)->get());
     }
 }
